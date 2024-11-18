662fb5de198e2128e2d1349dde28dc9a0b4f2e6a
AryanMegh <Aryanshivgunde9999@gmail.com>
2024-11-18 20:00:32 +0530

    Add files via upload

---
+++ Lab1_Code_1.py
@@ -0,0 +1,15 @@
+'''
+Q1) Write a program to calculate simple interest?
+'''
+
+#Taking values from user to calcuate simple interest
+
+Principal_amount = int( input( " Enter principal amount: " ) )
+Rate_of_interest = int( input( " Enter rate of interest: " ) )
+Time_period = int( input( " Enter time period till time you want to keep mony in the bank: " ) )
+
+#A formula to calculate simple interest
+Simple_interest = (Principal_amount * Rate_of_interest * Time_period)/100
+
+#Printing the simple interest using print statement
+print( " The simple interest of ", Principal_amount, "with rate of interest", Rate_of_interest, "with", Time_period, "period of time is:", Simple_interest, "%" )
---
+++ Lab1_Code_2.py
@@ -0,0 +1,12 @@
+'''
+Q2) Write a program to get area of rectangle?
+'''
+#Decleartion of variable of integer type to take value from user to calcuate rectangle area
+Length = int( input( " Enter length of rectangle: " ) )
+Breadth = int( input( " Enter width of rectangle: " ) )
+
+#Formula to calculate rectangle area.
+Area = Length * Breadth
+
+#Using print statement to print area of rectangle.
+print( " Area of rectangle = ", Area )
